bb982bb8d98ddf29802fae23fcacfdb68414bdfb
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-02-08 14:15:11 +0800

    tech exam mp3 player

---
+++ app.js
@@ -0,0 +1,157 @@
+let currentMusic = 0;
+const music = document.querySelector('#audio');
+const songImage = document.querySelector('.img-song');
+const seekBar = document.querySelector('.seek-bar');
+const musicName = document.querySelector('.music-name');
+const artistName = document.querySelector('.artist-name');
+const currentTime = document.querySelector('.current-time');
+const songDuration = document.querySelector('.song-duration');
+const playButton = document.querySelector('.play-btn');
+const forwardButton = document.querySelector('.fa-forward-step');
+const backwardButton = document.querySelector('.fa-backward-step');
+
+// // Assuming you have defined songs somewhere
+// const songs = [
+//     { name: 'Song 1', artist: 'Artist 1', path: 'song1.mp3', cover: 'cover1.jpg' },
+//     { name: 'Song 2', artist: 'Artist 2', path: 'song2.mp3', cover: 'cover2.jpg' },
+//     // Add more songs as needed
+// ];
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+    return `${min}:${sec}`;
+};
+music.addEventListener('loadedmetadata', () => {
+    seekBar.max = music.duration;
+    songDuration.innerHTML = formatTime(music.duration);
+});
+
+setInterval(()=>{
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime);
+}, 500)
+seekBar.addEventListener('change',()=>{
+    music.currentTime = seekBar.value;
+})
+
+const playMusic =() =>{
+    music.play();
+    playButton.classList.remove('pause');
+    songImage.classList.add('play')
+    
+}
+//forward button
+forwardButton.addEventListener('click',()=>{
+    if(currentMusic >=songs.length -1){
+        currentMusic= 0;
+    }else{
+        currentMusic++;
+    }
+    setMusic(currentMusic)
+    playMusic();
+})
+//backward button
+backwardButton.addEventListener('click',()=>{
+    if(currentMusic >=songs.length -1){
+        currentMusic= 0;
+    }else{
+        currentMusic--;
+    }
+    setMusic(currentMusic)
+    playMusic();
+})
+
+playButton.addEventListener('click', () => {
+    if (playButton.className.includes('pause')) {
+        music.play();
+    } else {
+        music.pause();
+    }
+    playButton.classList.toggle('pause');
+    songImage.classList.toggle('play');
+});
+
+const setMusic = (i) => {
+    seekBar.value = 0; // Set range slide value to 0
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+
+    musicName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    songImage.style.backgroundImage = `url('${song.cover}')`;
+
+    currentTime.innerHTML = '00:00';
+    // songDuration.innerHTML = formatTime(music.duration);
+    // seekBar.max = music.duration;
+};
+
+setMusic(0);
+
+
+
+// let currentMusic = 0;
+// const music = document.querySelector('.music-player');
+// const songImage = document.querySelector('.img-song');
+// const seekBar = document.querySelector('.seek-bar');
+// const musicName = document.querySelector('.music-name');
+// const artistName = document.querySelector('.artist-name');
+// const currentTime = document.querySelector('.current-time');
+// const songDuration = document.querySelector('.song-duration');
+// const playButton = document.querySelector('.play-btn');
+// const forwardButton = document.querySelector('.fa-forward-step');
+// const backwardButton = document.querySelector('.fa-backward-step');
+
+
+// playButton.addEventListener('click',()=>{
+//     if(playButton.className.includes('pause')){
+//         music.play();
+//     }else{
+//         music.pause();
+//     }
+//     playButton.classList.toggle('pause');
+//     songImage.classList.toggle('play');
+// })
+
+// const setMusic =(i) =>{
+//     seekBar.value = 0; //set range slide value to 0;
+//     let song = songs[i];
+//     currentMusic = i;
+//     music.src = song.path;
+
+//     musicName.innerHTML = song.name;
+//     artistName.innerHTML = song.artist;
+//     songImage.style.backgroundImage = `url('${song.cover}')`;
+
+//     currentTime.innerHTML = '00:00'
+//     setTimeout(()=>{
+//         seekBar.max = music.songDuration;
+//         songDuration.innerHTML = formatTime(music.songDuration)
+        
+//     },300);    
+
+// }
+// setMusic(0);
+
+// const formatTime = (time) =>{
+//     let min = Math.floor(time / 60);
+//     if (min < 10){
+//         min = `0${min}`;
+        
+//     }
+//     let sec = Math.floor(time % 60);
+//     if(sec < 10){
+//         sec = `0${sec}`
+//     }
+//     return `${min} : ${sec}`;
+// }
+
+
+
---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name:"Superproxy",
+        path:"asset/1-12 Superproxy.m4a",
+        artist:"Eraserheads",
+        cover: "img/Eheads_anthology.jpg"
+    },
+    {
+        name:"Awit Ng Kabataan",
+        path:"asset/Awit Ng Kabataan.m4a",
+        artist:"Rivermaya",
+        cover: "img/rivermaya.jpg" 
+    },
+    {
+        name:"Fix You",
+        path:"asset/04 Fix You.m4a",
+        artist:"Coldplay",
+        cover: "img/coldplay.png" 
+    },
+    {
+        name:"Radioactive (iTunes Session)",
+        path:"asset/002 Radioactive (iTunes Session).m4a",
+        artist:"Imagine Dragons",
+        cover: "img/imaginedragons.jpg" 
+    },
+]
